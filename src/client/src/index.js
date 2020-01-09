989e77547d19736dc47a47d62a346fb93a8cbcb2
MateuszCiupa <01131212@pw.edu.pl>
2020-01-09 13:00:35 +0100

    create-react-app init

---
+++ src/client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        Yet another app component
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
