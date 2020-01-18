61f7f6e329a5cd2c27a3a84ab2aeb5b58d5165d6
MateuszCiupa <01131212@pw.edu.pl>
2020-01-18 15:22:41 +0100

    racks as carousel init

--- src/client/src/layouts/Racks.jsx
+++ src/client/src/layouts/Racks.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
 
 export default () => (
-    <div>
-        yet another racks page?
-    </div>
-)
+    <Carousel controls>
+        <Carousel.Item>
+            <img
+                src={`${process.env.PUBLIC_URL}/img/rack_30.png`}
+                alt="Yet another rack_30.png" 
+            />
+            
+            <Carousel.Caption>
+                <h3>Cool rack name</h3>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+                src={`${process.env.PUBLIC_URL}/img/rack_42.png`} 
+                alt="Yet another rack_42.png"
+            />
+            
+            <Carousel.Caption>
+                <h3>Damn thats big rack</h3>
+            </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
+);
