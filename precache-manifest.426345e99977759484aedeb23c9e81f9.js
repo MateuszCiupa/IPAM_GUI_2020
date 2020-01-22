278da7f2f672945a5b473ccf93e1faf8bb8e044c
MateuszCiupa <01131212@pw.edu.pl>
2020-01-22 19:13:00 +0100

    Updates

---
+++ precache-manifest.426345e99977759484aedeb23c9e81f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a3f9da9b082c4f4fa1d9ea0dcc5d84",
+    "url": "/IPAM_GUI_2020/index.html"
+  },
+  {
+    "revision": "92750c3c405cc5e0f159",
+    "url": "/IPAM_GUI_2020/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "92750c3c405cc5e0f159",
+    "url": "/IPAM_GUI_2020/static/js/2.5dc620f1.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/IPAM_GUI_2020/static/js/2.5dc620f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e3c8859b69d7662d385",
+    "url": "/IPAM_GUI_2020/static/js/main.3b0724b1.chunk.js"
+  },
+  {
+    "revision": "0facc83889547c574b87",
+    "url": "/IPAM_GUI_2020/static/js/runtime-main.48f8c740.js"
+  }
+]);
