b7d1796053d5e71c1a79362f961c068b6332db7b
MateuszCiupa <01131212@pw.edu.pl>
2020-01-30 01:21:19 +0100

    header

--- src/client/src/layouts/components/TableWrapper.jsx
+++ src/client/src/layouts/components/TableWrapper.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, FormCheck } from 'react-bootstrap';
+import { Button, FormCheck, Jumbotron } from 'react-bootstrap';
 import StyledTable from './styled/StyledTable';
 import { Link } from 'react-router-dom';
 import { compose } from 'redux';
@@ -16,138 +16,141 @@ const TableWrapper = ({ firestoreData, collectionName, history }) => {
     const [addShow, setAddShow] = useState(false);
 
     return (
-        <StyledTable 
-            size="sm" 
-            responsive 
-            striped 
-            headers={tableFields.length}
-            linkHover={linkHover}
-        >
-            <AddModalWrapper 
-                show={addShow}
-                setShow={setAddShow}
-                collectionName={collectionName}
-            />
+        <>
+            <h1 style={{ float: 'right' }}>{collectionName}</h1>
+            <StyledTable 
+                size="sm" 
+                responsive 
+                striped 
+                headers={tableFields.length}
+                linkHover={linkHover}
+            >
+                <AddModalWrapper 
+                    show={addShow}
+                    setShow={setAddShow}
+                    collectionName={collectionName}
+                />
 
-            <thead>
-                <tr>
-                    <th>
-                        {edit ? (
-                            <Button 
-                                size="sm"
-                                variant="secondary"
-                                onClick={() => {
-                                    setEdit(false);
-                                    setSelected([]);
-                                }}
+                <thead>
+                    <tr>
+                        <th>
+                            {edit ? (
+                                <Button 
+                                    size="sm"
+                                    variant="secondary"
+                                    onClick={() => {
+                                        setEdit(false);
+                                        setSelected([]);
+                                    }}
+                                >
+                                    Cancel
+                                </Button>
+                            ) : (
+                                <Button 
+                                    size="sm"
+                                    onClick={() => setAddShow(true)}
+                                >
+                                    Add
+                                </Button>
+                            )}
+                        </th>
+
+                        {collections[collectionName].tableFields.map(header => (
+                            <th key={header}>{header[0].toUpperCase() + header.slice(1)}</th>
+                        ))}
+
+                        <th>
+                            {edit ? (
+                                <Button 
+                                    size="sm" 
+                                    variant="danger"
+                                    disabled={selected.length === 0}
+                                >
+                                    Delete
+                                </Button>
+                            ) : (
+                                <Button 
+                                    size="sm" 
+                                    variant="secondary"
+                                    onClick={() => setEdit(true)}
+                                >
+                                    Edit
+                                </Button>
+                            )}
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {firestoreData[collectionName] ? Object.entries(firestoreData[collectionName]).map(
+                        ([documentId, documentObject]) => (
+                            <tr 
+                                key={documentId}
+                                onClick={() => !linkHover && history.push(`/${collectionName}/${documentId}`)}
                             >
-                                Cancel
-                            </Button>
-                        ) : (
-                            <Button 
-                                size="sm"
-                                onClick={() => setAddShow(true)}
-                            >
-                                Add
-                            </Button>
-                        )}
-                    </th>
 
-                    {collections[collectionName].tableFields.map(header => (
-                        <th key={header}>{header[0].toUpperCase() + header.slice(1)}</th>
-                    ))}
+                                <td></td>
 
-                    <th>
-                        {edit ? (
-                            <Button 
-                                size="sm" 
-                                variant="danger"
-                                disabled={selected.length === 0}
-                            >
-                                Delete
-                            </Button>
-                        ) : (
-                            <Button 
-                                size="sm" 
-                                variant="secondary"
-                                onClick={() => setEdit(true)}
-                            >
-                                Edit
-                            </Button>
-                        )}
-                    </th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {firestoreData[collectionName] ? Object.entries(firestoreData[collectionName]).map(
-                    ([documentId, documentObject]) => (
-                        <tr 
-                            key={documentId}
-                            onClick={() => !linkHover && history.push(`/${collectionName}/${documentId}`)}
-                        >
-
-                            <td></td>
-
-                            {tableFields.map(field => (
-                                <td key={documentId+field}>
-                                    {
-                                        (() => {
-                                            if (!documentObject[field]) {
-                                                return '';
-                                            } else if (Array.isArray(documentObject[field])) {
-                                                return documentObject[field].length;
-                                            } else if (typeof documentObject[field] === 'object') {
-                                                if (firestoreData[documentObject[field].parent.path]) {
-                                                    if (firestoreData[documentObject[field].parent.path][documentObject[field].id]) {
-                                                        return (
-                                                            <Link 
-                                                                to={`/${documentObject[field].path}`}
-                                                                onMouseEnter={() => setLinkHover(true)}
-                                                                onMouseLeave={() => setLinkHover(false)}
-                                                            >
-                                                                {firestoreData
-                                                                    [documentObject[field].parent.path]
-                                                                    [documentObject[field].id]
-                                                                    [collections[documentObject[field].parent.path].linkField]
-                                                                }
-                                                            </Link>
-                                                        );
+                                {tableFields.map(field => (
+                                    <td key={documentId+field}>
+                                        {
+                                            (() => {
+                                                if (!documentObject[field]) {
+                                                    return '';
+                                                } else if (Array.isArray(documentObject[field])) {
+                                                    return documentObject[field].length;
+                                                } else if (typeof documentObject[field] === 'object') {
+                                                    if (firestoreData[documentObject[field].parent.path]) {
+                                                        if (firestoreData[documentObject[field].parent.path][documentObject[field].id]) {
+                                                            return (
+                                                                <Link 
+                                                                    to={`/${documentObject[field].path}`}
+                                                                    onMouseEnter={() => setLinkHover(true)}
+                                                                    onMouseLeave={() => setLinkHover(false)}
+                                                                >
+                                                                    {firestoreData
+                                                                        [documentObject[field].parent.path]
+                                                                        [documentObject[field].id]
+                                                                        [collections[documentObject[field].parent.path].linkField]
+                                                                    }
+                                                                </Link>
+                                                            );
+                                                        } else {
+                                                            return '';
+                                                        }
                                                     } else {
                                                         return '';
                                                     }
                                                 } else {
-                                                    return '';
+                                                    return documentObject[field];
                                                 }
-                                            } else {
-                                                return documentObject[field];
-                                            }
-                                        })()
-                                    }
-                                </td>
-                            ))}
-
-                            <td>
-                                {edit ? (
-                                    <FormCheck 
-                                        type="checkbox"
-                                        onClick={() => 
-                                            setSelected(prev => 
-                                                prev.includes(documentId) ? 
-                                                prev.filter(id => id !== documentId) : 
-                                                prev.concat(documentId)
-                                            )
+                                            })()
                                         }
-                                        onMouseEnter={() => setLinkHover(true)}
-                                        onMouseLeave={() => setLinkHover(false)}
-                                    />
-                                ) : null}
-                            </td>
-                        </tr>
-                    )
-                ) : null}
-            </tbody>
-        </StyledTable>
+                                    </td>
+                                ))}
+
+                                <td>
+                                    {edit ? (
+                                        <FormCheck 
+                                            type="checkbox"
+                                            onClick={() => 
+                                                setSelected(prev => 
+                                                    prev.includes(documentId) ? 
+                                                    prev.filter(id => id !== documentId) : 
+                                                    prev.concat(documentId)
+                                                )
+                                            }
+                                            onMouseEnter={() => setLinkHover(true)}
+                                            onMouseLeave={() => setLinkHover(false)}
+                                        />
+                                    ) : null}
+                                </td>
+                            </tr>
+                        )
+                    ) : null}
+                </tbody>
+            </StyledTable>
+        </>
     );
 };
 
